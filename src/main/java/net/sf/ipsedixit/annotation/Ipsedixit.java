ae2a9baeb9ad5e17561ef5402696b3e7c7c210c1
Jason Royals <ipsedixit@fragstealers.com>
2010-11-23 06:39:16 +0000

    Bug 3116276

--- src/main/java/net/sf/ipsedixit/annotation/Ipsedixit.java
+++ src/main/java/net/sf/ipsedixit/annotation/Ipsedixit.java
@@ -17,6 +17,7 @@
 package net.sf.ipsedixit.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -29,6 +30,7 @@ import net.sf.ipsedixit.core.impl.NonFinalFieldObjectAnalyser;
  */
 @Target({ElementType.TYPE, ElementType.PACKAGE})
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
 public @interface Ipsedixit {
 
     /**
